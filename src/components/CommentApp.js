960525ff14c479b2ea6d3611f6970729ddbd921b
zhufengnodejs <zhufengnodejs@126.com>
2017-07-20 16:06:51 +0800

    test

--- src/components/CommentApp.js
+++ src/components/CommentApp.js
@@ -3,6 +3,14 @@ import 'bootstrap/dist/css/bootstrap.css'
 import CommentInput from "./CommentInput";
 import CommentList from "./CommentList";
 export default class CommentApp extends Component{
+  constructor(){
+    super();
+    //在两个子组件共同的父组件中定义一个状态对象。comments数组表示留言列表
+    this.state = {comments:[
+      {id:1,user:'张三',content:'今天天气真好!',createAt:new Date()},
+      {id:2,user:'李四',content:'天气不错!',createAt:new Date()}
+    ]};
+  }
   render(){
     return (
       <div className="container" style={{marginTop:20}}>
@@ -13,7 +21,7 @@ export default class CommentApp extends Component{
                 <h3 className="text-center">欢迎来到珠峰留言版</h3>
               </div>
               <div className="panel-body">
-                <CommentList/>
+                <CommentList comments = {this.state.comments}/>
               </div>
               <div className="panel-footer">
                 <CommentInput/>
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import Comment from "./Comment";
+//组件状态只能内部初始化，内部改变，外部改变不了
 export default class CommentList extends Component{
     render(){
         return (
