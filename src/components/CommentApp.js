94782fc2216f1bddb59504d289b95c1b99d9893d
zhufengnodejs <zhufengnodejs@126.com>
2017-07-20 15:49:50 +0800

    test

--- src/components/CommentApp.js
+++ src/components/CommentApp.js
@@ -1,12 +1,26 @@
 import React,{Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
 import CommentInput from "./CommentInput";
 import CommentList from "./CommentList";
 export default class CommentApp extends Component{
   render(){
     return (
-      <div>
-        <CommentInput/>
-        <CommentList/>
+      <div className="container" style={{marginTop:20}}>
+        <div className="row">
+          <div className="col-md-8 col-md-offset-2">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="text-center">欢迎来到珠峰留言版</h3>
+              </div>
+              <div className="panel-body">
+                <CommentList/>
+              </div>
+              <div className="panel-footer">
+                <CommentInput/>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
