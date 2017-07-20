65545512bd6c136e459e4bad4678cd1b4cb0085f
zhufengnodejs <zhufengnodejs@126.com>
2017-07-20 16:26:50 +0800

    test

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,15 +1,21 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import Comment from "./Comment";
 //组件状态只能内部初始化，内部改变，外部改变不了
-export default class CommentList extends Component{
-    render(){
-        return (
-            <div>
-              <Comment/>
-              <Comment/>
-              <Comment/>
-              <Comment/>
-            </div>
-        )
-    }
+export default class CommentList extends Component {
+  render() {
+    return (
+      <ul className="list-group">
+        {
+          this.props.comments.map((item, index) => (
+            <li key={item.id} className="list-group-item">
+              {item.user}:{item.content}
+              <span className="pull-right">
+                {item.createAt.toLocaleString()}
+                </span>
+            </li>
+          ))
+        }
+      </ul>
+    )
+  }
 }
